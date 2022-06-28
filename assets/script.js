14a4773a50b28f53327f7178494b7e69bdd85f58
darmstrong <armstrong2579@gmail.com>
2022-06-28 10:34:03 -0700

    add comments

--- assets/script.js
+++ assets/script.js
@@ -22,3 +22,10 @@ startBtn.addEventListener("click", function quizGo(){
         } , 1000 );
 });
 
+// Setting up the minus ten function needs to be called when the wrong answer is chosen
+// input is false or question false or some shit idk i haven't used forms
+quizTimer = quizTimer - 10;
+
+// likely need to replace current HTML and add innerHTML to make it delete once quiz begins
+// can use document.innerHTML = ""; or some shit to clear maybe
+
