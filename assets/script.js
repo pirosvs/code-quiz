6ec992f3f93cfa1e7ad05fe987284969d5b90859
darmstrong <armstrong2579@gmail.com>
2022-06-28 18:43:57 -0700

    add buttons per correct response

--- assets/script.js
+++ assets/script.js
@@ -28,6 +28,40 @@ startBtn.addEventListener("click", function quizGo(){
         } , 1000 );
 });
 
+var answerBtnA = document.getElementById("questionButta")
+
+answerBtnA.addEventListener("click", function answerCheckA(){
+  if(answerBtnA.value == QuizContent[0].correctAnswer){
+    score += 1;
+    console.log("correct");
+  } else {
+    quizTimer -= 10;
+    console.log("incorrect");
+  }
+});
+
+var answerBtnB = document.getElementById("questionButtb")
+
+answerBtnB.addEventListener("click", function answerCheckB(){
+  if(answerBtnB.value == QuizContent[0].correctAnswer){
+    score += 1;
+    console.log("correct");
+  } else {
+    quizTimer -= 10;
+    console.log("incorrect")
+  }
+});
+
+var answerBtnC = document.getElementById("questionButtc")
+
+answerBtnC.addEventListener("click", function answerCheckC(){
+  if(answerBtnC.value == QuizContent[0].correctAnswer){
+    score += 1;
+  } else {
+    quizTimer -= 10;
+  }
+});
+
 // Setting up the minus ten function needs to be called when the wrong answer is chosen
 // input is false or question false or some shit idk i haven't used forms
 quizTimer = quizTimer - 10;
